01d41e0eeec50257799143fb75cb167d893279dd
Ibrahim Dahir <khaliilxub@gmail.com>
2025-07-22 15:58:13 +0300

    Configuring docker compose and wait for db

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Simple calculator app.
+"""
+
+
+def add(x, y):
+    """Add two numbers and return the result."""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract x from y and return the result."""
+    return y - x
---
+++ app/app/tests.py
@@ -0,0 +1,19 @@
+"""
+Simple tests for calculator.
+"""
+
+from django.test import SimpleTestCase
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+
+    def test_add_numbers(self):
+        """Test adding numbers."""
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """Test subtracting numbers."""
+        res = calc.subtract(10, 15)
+        self.assertEqual(res, 5)
